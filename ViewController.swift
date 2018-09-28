9ab28777d86466f8c64857ca3ff701aceae0b264
sam-9907 <42326756+sam-9907@users.noreply.github.com>
2018-09-28 15:57:25 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Proyectobueno
+//
+//  Created by macbook on 28/09/18.
+//  Copyright © 2018 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+
+}
+
